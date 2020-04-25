049eb2560395d0b3a7fadfe32e6994efe512130c
mahdislimane <slimane_mahdi@yahoo.fr>
2020-04-25 21:57:04 +0100

    Updates

---
+++ precache-manifest.ee82951a963fe66cc678b028c095a542.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11122452073c745d61d263f09a8e00c2",
+    "url": "/reduxcheck/index.html"
+  },
+  {
+    "revision": "5f0295bf2e45da011a0f",
+    "url": "/reduxcheck/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "df4ff6299a0bca371bce",
+    "url": "/reduxcheck/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5f0295bf2e45da011a0f",
+    "url": "/reduxcheck/static/js/2.59c2f08e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/reduxcheck/static/js/2.59c2f08e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df4ff6299a0bca371bce",
+    "url": "/reduxcheck/static/js/main.0005e239.chunk.js"
+  },
+  {
+    "revision": "e21f068123b7eab30e81",
+    "url": "/reduxcheck/static/js/runtime-main.2b5c9d48.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reduxcheck/static/media/logo.5d5d9eef.svg"
+  }
+]);
